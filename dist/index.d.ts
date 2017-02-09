baa3333f3c39375037790a97b776bbf542ffc8c1
Andrew Bell <andy.bell@informationage.co.nz>
2017-02-09 13:41:10 +1300

    Version Bump from 0.1.4 to 0.1.5

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,18 +1,18 @@
-// Typescript type definitions for mtype
-// Note: For vscode intellisense to work when developing d.ts files include jsconfig.json in project root
+// Typescript type definitions for mtype.
+// Note: For vscode intellisense to work when developing d.ts files include jsconfig.json in project root.
 // vscode intellisense unreliable in interpreting jsdoc @tags.  Abandoned @tags for simple text.
 
 /**
  *Applies test to subject and Returns: boolean result.
  */
-export interface typePredicate {
+interface typePredicate {
   (subject: any): boolean
 }
 
 /**
- *Key/Predicate function  pairs where keys are typeNames and predicate functions test whether subject is of that typeName
+ *Key/Predicate function  pairs where keys are typeNames and predicate functions test whether subject is of that typeName.
  */
-export interface typePredicates {
+interface typePredicates {
   [typeName: string]: typePredicate
 }
 
@@ -21,19 +21,19 @@ export interface typePredicates {
 /**
  *mtype factory function Returns: instance of api.  Api functions apply typePredicates against passed subject values.
  */
-export interface mtypeApi {
+interface mtypeApi {
   /**
    *Subject passes predicate test for typeName.
    *Param: subject Value to test against typeName predicate.
-   *Param: typeName Name of predicate test to apply to subject
-   *Returns: true if subject passes predicate test for typeName .
+   *Param: typeName Name of predicate test to apply to subject.
+   *Returns: true if subject passes predicate test for typeName.
    */
   is: (subject: any, typeName: string) => boolean
 
   /**
-   *Subject passes predicate test for typeName only. (Does NOT pass any other predicate tests)
+   *Subject passes predicate test for typeName only. (Does NOT pass any other predicate tests).
    *Param: subject Value to test against typeName predicate.
-   *Param: typeName Name of predicate test to apply to subject
+   *Param: typeName Name of predicate test to apply to subject.
    *Returns: true if subject passes predicate test for typeName and NO other predicate tests.
    */
   isOnly: (subject: any, typeName: string) => boolean
@@ -41,7 +41,7 @@ export interface mtypeApi {
   /**
    *Subject passes predicate test for a least one of the typeNames.
    *Param: subject Value to test against typeName predicates.
-   *Param: typeNames Array of names of the predicate tests to apply to subject
+   *Param: typeNames Array of names of the predicate tests to apply to subject.
    *Returns: true if subject passes predicate test for at least one of typeNames.
    */
   some: (subject: any, ...typeNames: string[]) => boolean
@@ -49,7 +49,7 @@ export interface mtypeApi {
   /**
    *Subject passes predicate tests for all the typeNames.
    *Param: subject Value to test against typeName predicates.
-   *Param: typeNames Array of names of the predicate tests to apply to subject
+   *Param: typeNames Array of names of the predicate tests to apply to subject.
    *Returns: true if subject passes predicate test for all typeNames.
    */
   every: (subject: any, ...typeNames: string[]) => boolean
@@ -69,7 +69,7 @@ export interface mtypeApi {
 
 
 /**
- *Get predicate typeNames
+ *Get predicate typeNames.
  *Returns: all predicate typeNames.
  */
   types: string[]
